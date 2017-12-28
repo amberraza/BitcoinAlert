6bc9fac3b8dfe3d5533ae27a1d66bc98663dec20
amberraza <amberraza.cs@gmail.com>
2017-12-28 02:27:48 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BitcoinAlert
-Bitcoin price alert to your phone
+Bitcoin price alert is sent to your phone whenever the price goes down the given threshold. The job is scheduled to run every hour. Following are the steps to send the alert:
+  - Import the twilio Client class from twilio.rest module and BlockingScheduler class from apscheduler module.
+  - Initialize the account id and authorization token given by twilio.
+  - Get the current price via google search.
+  - Initialize the client for the alert.
+  - Check for the threshold value.
+  - Send the alert to the given phone if the condition is met.
+  - Schedule the job.
+  - Start the scheduler.
---
+++ sendAlert.py
@@ -0,0 +1,31 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from twilio.rest import Client
+import requests
+import re
+
+
+# Your Account SID from twilio.com/console
+account_sid = ""
+# Your Auth Token from twilio.com/console
+auth_token  = ""
+
+def get_prices():
+    r = requests.get("https://google.com/search?q=bitcoin+price",stream=True)
+    data = r.text
+    priceIndex = re.search(r'\b(1 Bitcoin)\b',data)
+    price = data[priceIndex.start()+12:priceIndex.start()+20]
+    print(price)
+
+    client = Client(account_sid, auth_token)
+
+    if(price<'13000'):
+        message = client.messages.create(
+            #phone number to send to
+            to="",
+            #phone number given by twilio
+            from_="",
+            body="1 Bitcoin = "+price)
+
+scheduler = BlockingScheduler()
+scheduler.add_job(get_prices, 'interval', hours=1)
+scheduler.start()
